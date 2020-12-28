80e8d0970325ac1d6ab2aad8c82e9c1517f0895c
Halit Ege Ebiller <76678234+halitegeebiller@users.noreply.github.com>
2020-12-28 22:36:07 +0300

    Create app.R

---
+++ app.R
@@ -0,0 +1,69 @@
+library(shiny)
+library(ggplot2)
+library(datasets)
+
+mpgData <- mtcars
+mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
+
+
+# Define UI for miles per gallon app ----
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Miles Per Gallon"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Selector for variable to plot against mpg ----
+      selectInput("variable", "Variable:",
+                  c("Cylinders" = "cyl",
+                    "Transmission" = "am",
+                    "Gears" = "gear")),
+      
+      
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output: Formatted text for caption ----
+      h3(textOutput("caption")),
+      
+      # Output: Plot of the requested variable against mpg ----
+      plotOutput("mpgPlot")
+      
+    )
+  )
+)
+
+# Define server logic to plot various variables against mpg ----
+server <- function(input, output) {
+  
+  # Compute the formula text ----
+  # This is in a reactive expression since it is shared by the
+  # output$caption and output$mpgPlot functions
+  formulaText <- reactive({
+    paste("mpg ~", input$variable)
+  })
+  
+  # Return the formula text for printing as a caption ----
+  output$caption <- renderText({
+    formulaText()
+  })
+  
+  # Generate a plot of the requested variable against mpg ----
+  # and only exclude outliers if requested
+
+  output$mpgPlot <- renderPlot({
+    ggplot(mpgData, aes(x = mpg)) + geom_histogram(binwidth = 5, fill="steelblue") + facet_wrap(~mpgData[[input$variable]], nrow = 3)
+  })
+  
+}
+
+# Create Shiny app ----
+shinyApp(ui, server)
